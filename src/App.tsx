15a36fdc568698b9cfe4c2fa81e7ad8baf160ad1
himanigupta <gupta.himani44@gmail.com>
2024-12-18 04:26:26 -0800

    Add routing for basic pages: Home, NotFound, SignIn, SignUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/Not-Found";
+import HomePage from "./pages/Home";
 
 export default function Home() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<></>}></Route>
-          <Route path="/sign-in" element={<></>}></Route>
-          <Route path="/sign-up" element={<></>}></Route>
-          <Route path="/*" element={<></>}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
     </>
