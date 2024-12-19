07d91488a79e7b483b1479d8e488781b37236177
himanigupta <gupta.himani44@gmail.com>
2024-12-19 01:21:38 -0800

    Style SignIn page and link it to SignUp

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <>
+      {/* second step */}
+      <div className="flex items-center justify-center h-screen px-2">
+        {/* first step */}
+        <div className="flex flex-col space-y-4 w-full max-w-[400px]  bg-gray-300 p-4 rounded-lg">
+          <h3 className="text-center text-2xl font-bold mb-4">Sign In</h3>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Email"
+              className="bg-background"
+            />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="bg-background"
+            />
+          </div>
+          <Button>Sign in</Button>
+          <div>
+            <p>
+              Don't have an account?
+              <Link to="/sign-up" className="text-blue-800 font-semibold">
+                Sign Up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SignIn;
