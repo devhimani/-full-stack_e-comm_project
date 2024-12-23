6b4719225df7f2be80a8afecfc1f981f08683c3b
himanigupta <gupta.himani44@gmail.com>
2024-12-22 23:08:09 -0800

    Set up Formik in SignUp page and install Yup for validation

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,14 +2,28 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
+import { useFormik } from "formik";
 
 const SignUp = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <>
       {/* second step */}
       <div className="flex items-center justify-center h-screen px-2">
         {/* first step */}
-        <div className="flex flex-col space-y-4 w-full max-w-[400px]  bg-gray-300 p-4 rounded-lg">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex flex-col space-y-4 w-full max-w-[400px]  bg-gray-300 p-4 rounded-lg"
+        >
           <h3 className="text-center text-2xl font-bold mb-4">Sign Up</h3>
           <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="name">Name</Label>
@@ -18,6 +32,8 @@ const SignUp = () => {
               id="name"
               placeholder="Name"
               className="bg-background"
+              value={formik.values.name}
+              onChange={formik.handleChange}
             />
           </div>
           <div className="grid w-full max-w-sm items-center gap-1.5">
@@ -27,6 +43,8 @@ const SignUp = () => {
               id="email"
               placeholder="Email"
               className="bg-background"
+              value={formik.values.email}
+              onChange={formik.handleChange}
             />
           </div>
           <div className="grid w-full max-w-sm items-center gap-1.5">
@@ -36,9 +54,11 @@ const SignUp = () => {
               id="password"
               placeholder="Password"
               className="bg-background"
+              value={formik.values.password}
+              onChange={formik.handleChange}
             />
           </div>
-          <Button>Sign Up</Button>
+          <Button type="submit">Sign Up</Button>
           <div>
             <p>
               Already have an account?
@@ -47,7 +67,7 @@ const SignUp = () => {
               </Link>
             </p>
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
